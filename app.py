ef9fdfb11f084ca1e42b30aa06d401713fa6e0e9
sarahmadhusudhan1504-cyber <sarahmadhusudhan1504@gmail.com>
2026-03-23 12:22:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="A1HnZv16*",
+    database="portfolio"
+)
+
+cursor = db.cursor()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    query = "INSERT INTO contacts (name, email, message) VALUES (%s, %s, %s)"
+    values = (name, email, message)
+
+    cursor.execute(query, values)
+    db.commit()
+
+    return "Form Submitted Successfully!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
